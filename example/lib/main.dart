510fcf397151fa1d8dd36140254e81366a3df56b
Elijah <elijah.@Elijahs-MacBook-Air.local>
2025-02-20 14:05:06 +0300

    changed USD to IQD

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
                 final payment = await fibPayment.createPayment(
                   PaymentRequest(
                     amount: '100.00',
-                    currency: 'USD',
+                    currency: 'IQD',
                     description: 'Test Payment',
                   ),
                   token,
